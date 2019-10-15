8d461cb5a014d1014032aa5c18c7145ca1a04960
jfmantova <jf.mantova@gmail.com>
2019-10-14 23:06:59 -0300

    Created shell-bot.py
    A command-line bot intended to pilote a raspberry pi as it talks to you.

---
+++ shell-bot.py
@@ -0,0 +1,10 @@
+prompt = '<< shell-bot >>: '
+def init():
+    while True:
+        input = input(prompt)
+        if input === 'exit'
+            break
+        else 
+            print('Not found')
+   
+init()
